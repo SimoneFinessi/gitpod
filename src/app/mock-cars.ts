6d04e86f7feb4dd176e8eec677749a03c5f45681
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2023-02-10 12:35:56 +0000

    creazione_file

--- src/app/mock-cars.ts
+++ src/app/mock-cars.ts
@@ -4,6 +4,6 @@ export const CARS: Car[] = [
   { id: 11, name: 'Putno', costo:10, consumo:25 },
   { id: 12, name: 'Bravo', costo:20, consumo:20 }, 
   { id: 13, name: 'Tipo', costo:15, consumo:10 }, 
-  { id: 14, name: '124_spider' , costo:100, consumo:90 },
-  { id: 15, name: '500', costo:1000, consumo:33 }
+  { id: 14, name: 'subaru_baracca' , costo:100, consumo:90 },
+  { id: 15, name: 'lamborghini', costo:100000000000, consumo:33 }
 ];
